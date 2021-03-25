5a2e18db818339dbd04f982367483ec3b8857ff3
t108590009 <t108590009@ntut.org.tw>
2021-03-25 15:12:41 +0800

    ball的keydown判定跟combo計數大致完成了

--- html5_game3.3/OOPL_Project/melody's escape/game_sample/js/level1.js
+++ html5_game3.3/OOPL_Project/melody's escape/game_sample/js/level1.js
@@ -1,96 +1,132 @@
 var Level1 = Framework.Class(Framework.Level, {
-		load : function () {
-			this.ball=new Array();
-			for(i=0;i<4;i++){
-				this.ball[i]=new Array();
-				for(j=0;j<5;j++){
-					this.ball[i][j]=new Ball();
-					this.ball[i][j].init('ball'+i.toString()+'.png');
-					this.ball[i][j].position={x:-999,y:500};
-				}
-			}//這邊4跟5的寫法看起來很髒,之後再改掉
+    load: function() {
+        this.ball = new Array();
+        for (i = 0; i < 4; i++) {
+            this.ball[i] = new Array();
+            for (j = 0; j < 5; j++) {
+                this.ball[i][j] = new Ball();
+                this.ball[i][j].init('ball' + i.toString() + '.png');
+                this.ball[i][j].position = {
+                    x: -999,
+                    y: 500
+                };
+            }
+        }
+        //這邊4跟5的寫法看起來很髒,之後再改掉
 
+        this.character = new Character();
+        this.character.init('run.png');
+        this.character.position = {
+            x: 300,
+            y: 450
+        };
+        this.character.scale = 0.6
+        this.character.run();
 
+        beatsCounter = 0;
+        this.sheet = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3];
+        this.tempo = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
+        this.combo = 0;
+        this.maxCombo = 0;
 
-			this.character=new Character();
-			this.character.init('run.png');
-			this.character.position={x:300,y:450};
-			this.character.scale=0.6
-			this.character.run();
+    },
 
-			beatsCounter=0;
-            this.sheet=[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3];
-            this.tempo=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
+    initialize: function() {
+        this.rootScene.attach(this.character.pic);
 
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                this.rootScene.attach(this.ball[i][j].pic);
+            }
+        }
+        //這邊4跟5的寫法看起來很髒,之後再改掉
 
-		},
-		
-		initialize : function () {
-			this.rootScene.attach(this.character.pic);
-			
-			for(i=0;i<4;i++){
-				for(j=0;j<5;j++){
-					this.rootScene.attach(this.ball[i][j].pic);
-				}
-			}//這邊4跟5的寫法看起來很髒,之後再改掉
+    },
 
-		},
+    click: function(e) {
+        beatsCounter = 0;
+        this.combo = 0;
+        hitsQueue = [];
+        //這是一個用來存畫面上的ball的方向與編號的queue,裡面的成員都是陣列[dirc,number]
 
+        this.startTime = Date.now();
+    },
+    //測試能不能把球拿出來跑,之後會把整個click註解掉
 
-		click: function(e){
-			beatsCounter=0;
-			
-			hitsQueue=[];//這是一個用來存畫面上的ball的queue。
+    keydown: function(e) {
 
-			this.startTime=Date.now();
-		},//測試能不能把球拿出來跑,之後會把整個click註解掉
+        var timePassed = Date.now() - this.startTime;
+        if (e) {//這邊判定還需要修改,不然遊戲還沒開始時亂按會跳錯
+            var comingBall=this.ball[hitsQueue[0][0]][[hitsQueue[0][1]]];
+            const keyConv=['S','D','A','W'];
+            if (comingBall.position.x < this.character.position.x + 150&&e.key===keyConv[hitsQueue[0][0]]) {
+                //150到-50是判定的range,手感跟原本遊戲差很多...待持續優化
+                //keyConv[hitsQueue[0][0]]是來的這顆球的方向
+                this.combo++;
+                console.log(this.combo);
+                comingBall.position = {
+                       x: -999,
+                       y: 500
+                };
+                hitsQueue.shift();
+                if (this.combo > this.maxCombo) {
+                   this.maxCombo = this.combo;
+                }
+             }else{
+                this.combo = 0;
+                console.log("Error");
+             }
+        }
+    },
 
-        keydown:function(e){
+    update: function() {
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                this.ball[i][j].update();
+            }
+        }
+        //這邊4跟5的寫法看起來很髒,之後再改掉
 
-			var timePassed=Date.now()-this.startTime;
+        if (this.startTime > 0) {
+            var timePassed = Date.now() - this.startTime;
 
-			if(e.key==='S'){
-                console.log(hitsQueue[0].position.x);
-        	}
-        },
+            if (timePassed > this.tempo[beatsCounter] * 750) {
+                //之後會弄個bpm的const。這個*750的動作應該要在init時做完,否則影響遊戲順暢。
+                dirc = this.sheet[beatsCounter];
+                beatsCounter++;
 
+                for (i = 0; i < this.ball[dirc].length; i++) {
+                    //這個for的功能是丟出一個球,dirc是球的方向
+                    if (this.ball[dirc][i].position.x < 0) {
+                        //代表this.ball[dirc][i]不在畫面內
+                        this.ball[dirc][i].start();
+                        hitsQueue.push([dirc,i]);
+                        break;
+                    }
+                }
 
-		update : function () {
-			for(i=0;i<4;i++){
-				for(j=0;j<5;j++){
-					this.ball[i][j].update();
-				}
-			}//這邊4跟5的寫法看起來很髒,之後再改掉
+            }
 
-            if(this.startTime>0){
-		    	var timePassed=Date.now()-this.startTime;
+            if (hitsQueue.length > 0) {
+                var comingBall=this.ball[hitsQueue[0][0]][[hitsQueue[0][1]]];
+                if (comingBall.position.x != -999 && comingBall.position.x < this.character.position.x - 50) {
+                    console.log("Too late");
+                    this.combo = 0;
+                    comingBall.position = {
+                        x: -999,
+                        y: 500
+                    };
+                    hitsQueue.shift();
+                    //不能直接寫.position.x=-999
+                }
+            }
+        }
 
-		    	if (timePassed>this.tempo[beatsCounter]* 750 ){//之後會弄個bpm的const。這個*750的動作應該要在init時做完,否則影響遊戲順暢。
-	    			dirc=this.sheet[beatsCounter];
-	    			beatsCounter++;
+        //console.log(Date.now());
+    },
 
-	        		for(i=0;i<this.ball[dirc].length;i++){//這個for的功能是丟出一個球,dirc是球的方向
-		        		if(this.ball[dirc][i].position.x<0){//代表第i顆不在畫面內
-		    	    		this.ball[dirc][i].start();
-		    	    		hitsQueue.push(this.ball[dirc][i]);
-				    	break;
-		    		    }
-		        	}
+    draw: function(parentCtx) {
+        this.rootScene.draw();
+    },
 
-		    	}
-
-                if(hitsQueue.length>0)
-			        if(hitsQueue[0].position.x!=-999&&hitsQueue[0].position.x<this.character.position.x-50){
-			        	console.log("Too late");
-			        	hitsQueue.shift().position={x:-999,y:500};//不能直接寫.position.x=-999
-		        	}
-			}
-
-			//console.log(Date.now());
-		},
-
-		draw : function (parentCtx) {
-			this.rootScene.draw();
-		},
-		
-	});
+});

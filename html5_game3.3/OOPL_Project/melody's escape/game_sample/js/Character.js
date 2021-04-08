0ca4f8cf26d072191cab16466bdb99a694c56a50
t108590009 <t108590009@ntut.org.tw>
2021-04-09 01:12:00 +0800

    新增滑鏟、前踢的動作,圖片還沒處理好。

--- html5_game3.3/OOPL_Project/melody's escape/game_sample/js/Character.js
+++ html5_game3.3/OOPL_Project/melody's escape/game_sample/js/Character.js
@@ -24,12 +24,40 @@ var Character = function () {
             },
         });
 
-        this.init=function(sprite){
-            this.pic=new Framework.AnimationSprite({url:define.imagePath+sprite, col:3 , row:3 , loop:true , speed:2}); 
+        this.init=function(){
+            this.pic=new Framework.AnimationSprite({url:
+            [               
+                define.imagePath + 'jog.png',
+                define.imagePath + 'jog1.png',
+                define.imagePath + 'jog2.png',
+                define.imagePath + 'jog3.png',
+                define.imagePath + 'jog4.png',
+                define.imagePath + 'jog5.png',
+                define.imagePath + 'jog6.png',
+                define.imagePath + 'jog7.png',
+                define.imagePath + 'kick.png',
+                define.imagePath + 'kick1.png',
+                define.imagePath + 'kick2.png',
+                define.imagePath + 'kick3.png',
+                define.imagePath + 'kick4.png',
+                define.imagePath + 'kick5.png',
+                define.imagePath + 'slide.png',
+                define.imagePath + 'slide1.png',
+                define.imagePath + 'slide2.png',
+                define.imagePath + 'slide3.png',
+                define.imagePath + 'slide4.png'
+
+            ],loop:true , speed:2}); 
         };
-        this.run = function() {
+        this.jog = function() {
             this.pic.start({ from: 0, to:7, loop: true });
         };
+        this.kick=function(finishPlaying){
+            this.pic.start({ from: 7, to:13, loop: false,finishPlaying:finishPlaying });
+        }
+        this.slide=function(finishPlaying){
+            this.pic.start({ from: 13, to:18, loop: false,finishPlaying:finishPlaying });
+        }        
         this.update=function(){
                 
         }
--- html5_game3.3/OOPL_Project/melody's escape/game_sample/js/level1.js
+++ html5_game3.3/OOPL_Project/melody's escape/game_sample/js/level1.js
@@ -8,23 +8,25 @@ var Level1 = Framework.Class(Framework.Level, {
                 this.ball[i][j].init('ball' + i.toString() + '.png');
                 this.ball[i][j].position = {
                     x: -999,
-                    y: 500
+                    y: 550
                 };
             }
         }
         //這邊4跟5的寫法看起來很髒,之後再改掉
 
         this.character = new Character();
-        this.character.init('run.png');
+        this.character.init();
         this.character.position = {
             x: 300,
             y: 450
         };
-        this.character.scale = 0.6
-        this.character.run();
+        this.character.scale =1;
+        this.character.jog();
 
         beatsCounter = 0;
+        //this.sheet=[0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0];
         this.sheet = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        //this.tempo=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56];
         this.tempo = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,63,64,65,66,67,69,71,72,73,74,75,77,79,80,81,82,83,85,87,88,89,90,91,93];
         //這邊間隔2是指一拍(4分音符),間隔1指半拍,間隔4指2拍,以此類推。
         this.combo = 0;
@@ -63,15 +65,16 @@ var Level1 = Framework.Class(Framework.Level, {
     keydown: function(e) {
 
         var timePassed = Date.now() - this.startTime;
-        if (e) {//這邊判定還需要修改,不然遊戲還沒開始時亂按會跳錯
+        const keyConv=['S','D','A','W'];
+        const keyrConv=['Down','Right','Left','Up'];
+        if (e&&keyConv.includes(e.key)) {
             var comingBall=this.ball[hitsQueue[0][0]][[hitsQueue[0][1]]];
-            const keyConv=['S','D','A','W'];
             if (comingBall.position.x < this.character.position.x + 150) {
                 //150到-50是判定的range,手感跟原本遊戲差很多...待持續優化
                 //keyConv[hitsQueue[0][0]]是來的這顆球的方向
                 comingBall.position = {
                             x: -999,
-                            y: 500
+                            y: 550
                 };
                 if(e.key===keyConv[hitsQueue[0][0]]){
                     this.combo++;
@@ -90,6 +93,19 @@ var Level1 = Framework.Class(Framework.Level, {
                 console.log("Error");
              }
         }
+        if(e&&keyrConv.includes(e.key)){
+            var game=this;
+            if(e.key==="Right"||e.key==="Up"){
+                this.character.kick(function(){
+                    game.character.jog();
+                });
+            }
+            if(e.key==="Down"){
+                this.character.slide(function(){
+                    game.character.jog();
+                });
+            }
+        }
     },
 
     update: function() {
@@ -103,7 +119,8 @@ var Level1 = Framework.Class(Framework.Level, {
         if (this.startTime > 0) {
             var timePassed = Date.now() - this.startTime;
 
-            if (timePassed > this.tempo[beatsCounter] * 500) {
+            if (timePassed >= this.tempo[beatsCounter] * 250) {
+
                 //之後會弄個bpm的const。這個this.tempo全部*250的動作應該要在init時做完,否則影響遊戲順暢。
                 //bpm是beats per minutes,一首120bpm的歌的8分音符應該是1秒2拍,1拍0.5秒,半拍則是250ms。
                 dirc = this.sheet[beatsCounter];
@@ -128,7 +145,7 @@ var Level1 = Framework.Class(Framework.Level, {
                     this.combo = 0;
                     comingBall.position = {
                         x: -999,
-                        y: 500
+                        y: 550
                     };
                     hitsQueue.shift();
                     //不能直接寫.position.x=-999
